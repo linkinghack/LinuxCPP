a8196bb6bedc8bc77190fa5d7f2bbc66951ee1ea
linkinghack <linking@linkinghack.com>
2019-09-23 14:45:26 +0800

    DLL usage

--- Libraries/main.cpp
+++ Libraries/main.cpp
@@ -5,10 +5,23 @@ int test_func2(int);
 
 
 /**
+ * Static Library
  * g++ -c func1.cpp func2.cpp (compile library->func1.o, func2.o)
  * ar -cvq libcppfunc.a func1.o func2.o  (create static library libcppfunc.a)
  * ar -t libcppfunc.a (list files in library)
  * g++ -o mainexe main.cpp -L$(pwd) -lcppfunc (compile with static library)
+ * 
+ * Dynamically Linked Library
+ *  gcc -Wall -fPIC -c *.c
+ *  gcc -shared -Wl,-soname,libctest.so.1 -o libctest.so.1.0   *.o
+ * 
+ * Install Dynamically Linked Library
+ *  mv libctest.so.1.0 /opt/lib
+ *  ln -sf /opt/lib/libcppfunc.so.1.0 /opt/lib/libcppfunc.so.1
+ *  ln -sf /opt/lib/libcppfunc.so.1 /opt/lib/libcppfunc.so
+ * 
+ * Use DLL
+ *  g++  -Wall -I/path/to/include-files  -L/path/to/dll main.cpp -lcppfunc -o main
  * */
 int main() {
     int raw = 123;
